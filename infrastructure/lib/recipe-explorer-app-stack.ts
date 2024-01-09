35e08c1e2c41d5c0a968620028f0fe0949cda2be
Robb Charnock <robbchar@gmail.com>
2024-01-08 16:59:53 -0800

    typo

--- infrastructure/lib/recipe-explorer-app-stack.ts
+++ infrastructure/lib/recipe-explorer-app-stack.ts
@@ -1,21 +1,30 @@
-import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import {
+  IResource,
+  LambdaIntegration,
+  MockIntegration,
+  PassthroughBehavior,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { join } from 'path'
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { join } from "path";
 
 export class RecipeExplorerAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const dynamoTable = new Table(this, 'items', {
+    const dynamoTable = new Table(this, "items", {
       partitionKey: {
-        name: 'itemId',
-        type: AttributeType.STRING
+        name: "itemId",
+        type: AttributeType.STRING,
       },
-      tableName: 'items',
+      tableName: "items",
 
       /**
        *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
@@ -28,36 +37,40 @@ export class RecipeExplorerAppStack extends Stack {
     const nodeJsFunctionProps: NodejsFunctionProps = {
       bundling: {
         externalModules: [
-          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+          "aws-sdk", // Use the 'aws-sdk' available in the Lambda runtime
         ],
       },
-      depsLockFilePath: join(__dirname, '../../application/src/lambdas', 'package-lock.json'),
+      depsLockFilePath: join(
+        __dirname,
+        "../../application/src/lambdas",
+        "package-lock.json"
+      ),
       environment: {
-        PRIMARY_KEY: 'itemId',
+        PRIMARY_KEY: "itemId",
         TABLE_NAME: dynamoTable.tableName,
       },
       runtime: Runtime.NODEJS_20_X,
-    }
+    };
 
     // Create a Lambda function for each of the CRUD operations
-    const getOneLambda = new NodejsFunction(this, 'getOneItemFunction', {
-      entry: join(__dirname, '../../application/src/lambdas', 'get-one.ts'),
+    const getOneLambda = new NodejsFunction(this, "getOneItemFunction", {
+      entry: join(__dirname, "../../application/src/lambdas", "get-one.ts"),
       ...nodeJsFunctionProps,
     });
-    const getAllLambda = new NodejsFunction(this, 'getAllItemsFunction', {
-      entry: join(__dirname, '../../application/src/lambdas', 'get-all.ts'),
+    const getAllLambda = new NodejsFunction(this, "getAllItemsFunction", {
+      entry: join(__dirname, "../../application/src/lambdas", "get-all.ts"),
       ...nodeJsFunctionProps,
     });
-    const createOneLambda = new NodejsFunction(this, 'createItemFunction', {
-      entry: join(__dirname, '../../application/src/lambdas', 'create.ts'),
+    const createOneLambda = new NodejsFunction(this, "createItemFunction", {
+      entry: join(__dirname, "../../application/src/lambdas", "create.ts"),
       ...nodeJsFunctionProps,
     });
-    const updateOneLambda = new NodejsFunction(this, 'updateItemFunction', {
-      entry: join(__dirname, '../../application/src/lambdas', 'update-one.ts'),
+    const updateOneLambda = new NodejsFunction(this, "updateItemFunction", {
+      entry: join(__dirname, "../../application/src/lambdas", "update-one.ts"),
       ...nodeJsFunctionProps,
     });
-    const deleteOneLambda = new NodejsFunction(this, 'deleteItemFunction', {
-      entry: join(__dirname, '../../application/src/lambdas', 'delete-one.ts'),
+    const deleteOneLambda = new NodejsFunction(this, "deleteItemFunction", {
+      entry: join(__dirname, "../../application/src/lambdas", "delete-one.ts"),
       ...nodeJsFunctionProps,
     });
 
@@ -75,55 +88,64 @@ export class RecipeExplorerAppStack extends Stack {
     const updateOneIntegration = new LambdaIntegration(updateOneLambda);
     const deleteOneIntegration = new LambdaIntegration(deleteOneLambda);
 
-
     // Create an API Gateway resource for each of the CRUD operations
-    const api = new RestApi(this, 'itemsApi', {
-      restApiName: 'Items Service'
+    const api = new RestApi(this, "itemsApi", {
+      restApiName: "Items Service",
       // In case you want to manage binary types, uncomment the following
       // binaryMediaTypes: ["*/*"],
     });
 
-    const items = api.root.addResource('items');
-    items.addMethod('GET', getAllIntegration);
-    items.addMethod('POST', createOneIntegration);
+    const items = api.root.addResource("items");
+    items.addMethod("GET", getAllIntegration);
+    items.addMethod("POST", createOneIntegration);
     addCorsOptions(items);
 
-    const singleItem = items.addResource('{id}');
-    singleItem.addMethod('GET', getOneIntegration);
-    singleItem.addMethod('PATCH', updateOneIntegration);
-    singleItem.addMethod('DELETE', deleteOneIntegration);
+    const singleItem = items.addResource("{id}");
+    singleItem.addMethod("GET", getOneIntegration);
+    singleItem.addMethod("PATCH", updateOneIntegration);
+    singleItem.addMethod("DELETE", deleteOneIntegration);
     addCorsOptions(singleItem);
-  }
   }
 }
 
 export function addCorsOptions(apiResource: IResource) {
-  apiResource.addMethod('OPTIONS', new MockIntegration({
-    // In case you want to use binary media types, uncomment the following line
-    // contentHandling: ContentHandling.CONVERT_TO_TEXT,
-    integrationResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
-        'method.response.header.Access-Control-Allow-Credentials': "'false'",
-        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+  apiResource.addMethod(
+    "OPTIONS",
+    new MockIntegration({
+      // In case you want to use binary media types, uncomment the following line
+      // contentHandling: ContentHandling.CONVERT_TO_TEXT,
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers":
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+            "method.response.header.Access-Control-Allow-Credentials":
+              "'false'",
+            "method.response.header.Access-Control-Allow-Methods":
+              "'OPTIONS,GET,PUT,POST,DELETE'",
+          },
+        },
+      ],
+      // In case you want to use binary media types, comment out the following line
+      passthroughBehavior: PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": '{"statusCode": 200}',
       },
-    }],
-    // In case you want to use binary media types, comment out the following line
-    passthroughBehavior: PassthroughBehavior.NEVER,
-    requestTemplates: {
-      "application/json": "{\"statusCode\": 200}"
-    },
-  }), {
-    methodResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-      },
-    }]
-  })
+    }),
+    {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers": true,
+            "method.response.header.Access-Control-Allow-Methods": true,
+            "method.response.header.Access-Control-Allow-Credentials": true,
+            "method.response.header.Access-Control-Allow-Origin": true,
+          },
+        },
+      ],
+    }
+  );
 }
